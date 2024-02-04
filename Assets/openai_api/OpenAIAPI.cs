22ae9bf9a40c27cd98f6d985964f544992e1cdbc
Sunija <gothab@t-online.de>
2024-02-04 19:56:30 +0100

    streaming works

--- Assets/openai_api/OpenAIAPI.cs
+++ Assets/openai_api/OpenAIAPI.cs
@@ -24,13 +24,17 @@ public class OpenAIAPI : MonoBehaviour
         LLMRequest requestBody = new LLMRequest()
         {
             strPrompt = _strPrompt,
+            bStream = true,
             iMaxTokens = 50,
             fTemperature = 0.7f,
             fTopP = 0.9f,
             iSeed = Random.Range(0, int.MaxValue),
         };
 
-        string strJsonData = JsonConvert.SerializeObject(requestBody, Formatting.Indented);
+        string strJsonData = JsonConvert.SerializeObject(requestBody, Formatting.Indented, new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore
+        });
         Debug.Log($"Sending request: \n{strJsonData}");
 
         using (UnityWebRequest request = new UnityWebRequest(strApiUrl, "POST"))
@@ -41,21 +45,34 @@ public class OpenAIAPI : MonoBehaviour
             request.uploadHandler = new UploadHandlerRaw(bodyRaw);
             request.downloadHandler = new DownloadHandlerBuffer();
 
-
-            // Start the request asynchronously
             UnityWebRequestAsyncOperation asyncOperation = request.SendWebRequest();
             float fLastProgress = 0f;
 
-            // Continue updating progress until the request is completed
+            string strFullText = "";
+            int iLastIndex = 0;
+
             while (!asyncOperation.isDone)
             {
+                // for streaming: 
                 float fCurrentProgress = request.downloadProgress;
                 // Check if progress has changed
-                if (fCurrentProgress != fLastProgress) //&& callback != null)
+                if (fCurrentProgress != fLastProgress)
                 {
-                    //callback?.Invoke(ConvertContent(request.downloadHandler.text, getContent));
                     fLastProgress = fCurrentProgress;
-                    Debug.Log(request.downloadHandler.text);
+                    string strNewText = request.downloadHandler.text.Substring(iLastIndex);
+                    iLastIndex = request.downloadHandler.text.Length;
+
+                    string[] arMessages = strNewText.Split(new char[] { '\n' });
+                    List<LLMAnswer> liAnswers = new List<LLMAnswer>();
+
+                    foreach (string strMessage in arMessages)
+                    {
+                        if (strMessage.Length < 5) // sometimes last line is empty
+                            continue;
+                        string strMessagePruned = strMessage.Replace("data: ", ""); // TODO: make more performant
+                        LLMAnswer llmAnswer = JsonConvert.DeserializeObject<LLMAnswer>(strMessagePruned);
+                        _actionOnComplete?.Invoke(llmAnswer.liChoices[0].strText);
+                    }
                 }
                 yield return null;
             }
@@ -63,15 +80,22 @@ public class OpenAIAPI : MonoBehaviour
             if (request.result != UnityWebRequest.Result.Success)
                 throw new System.Exception(request.error);
 
-            Debug.Log(request.downloadHandler.text);
-            LLMAnswer llmAnswer = JsonConvert.DeserializeObject<LLMAnswer>(request.downloadHandler.text);
-            _actionOnComplete?.Invoke(llmAnswer.liChoices[0].strText);
+            // text_completion.chunk
+            // text_completion
+            if (requestBody.bStream != true)
+            {
+                Debug.Log("end: " + request.downloadHandler.text);
+                LLMAnswer llmAnswer = JsonConvert.DeserializeObject<LLMAnswer>(request.downloadHandler.text);
+                _actionOnComplete?.Invoke(llmAnswer.liChoices[0].strText);
+            }
+            
         }
-        
+
     }
 }
 
-[System.Serializable]
+
+/*
 public class LLMRequest
 {
     [JsonProperty("prompt")]
@@ -88,6 +112,173 @@ public class LLMRequest
     public float fMinP = 0.15f;
     [JsonProperty("seed")]
     public int iSeed = Random.Range(0, int.MaxValue);
+}
+*/
+
+[System.Serializable]
+public class LLMRequest
+{
+    [JsonProperty("model")]
+    public string strModel;
+
+    [JsonProperty("prompt")]
+    public string strPrompt;
+
+    //[JsonProperty("best_of")]
+    //public int iBestOf; // not used by ooba
+
+    [JsonProperty("echo")]
+    public bool? bEcho;
+
+    [JsonProperty("frequency_penalty")]
+    public float? fFrequencyPenalty;
+
+    [JsonProperty("logit_bias")]
+    public object? logitBias; // dictionary
+
+    [JsonProperty("logprobs")]
+    public int? logProbs;
+
+    [JsonProperty("max_tokens")]
+    public int? iMaxTokens = 50;
+
+    //[JsonProperty("n")]
+    //public int iN; // not used by ooba
+
+    [JsonProperty("presence_penalty")]
+    public float? fPresencePenalty;
+
+    [JsonProperty("stop")]
+    public List<string> strStop = new List<string>();
+
+    [JsonProperty("stream")]
+    public bool? bStream;
+
+    [JsonProperty("suffix")]
+    public string strSuffix;
+
+    [JsonProperty("temperature")]
+    public float? fTemperature;
+
+    [JsonProperty("top_p")]
+    public float? fTopP;
+
+    [JsonProperty("user")]
+    public string strUser;
+
+    //[JsonProperty("preset")]
+    //public string strPreset; // instruction preset, not used by us
+
+    [JsonProperty("min_p")]
+    public float? fMinP;
+
+    [JsonProperty("dynamic_temperature")]
+    public bool? bDynamicTemperature;
+
+    [JsonProperty("dynatemp_low")]
+    public float? fDynaTempLow;
+
+    [JsonProperty("dynatemp_high")]
+    public float? fDynaTempHigh;
+
+    [JsonProperty("dynatemp_exponent")]
+    public float? fDynaTempExponent;
+
+    [JsonProperty("top_k")]
+    public int? iTopK;
+
+    [JsonProperty("repetition_penalty")]
+    public float? repetitionPenalty;
+
+    [JsonProperty("repetition_penalty_range")]
+    public int? repetitionPenaltyRange;
+
+    [JsonProperty("typical_p")]
+    public float? typicalP;
+
+    [JsonProperty("tfs")]
+    public float? fTfs;
+
+    [JsonProperty("top_a")]
+    public float? fTopA;
+
+    [JsonProperty("epsilon_cutoff")]
+    public int? epsilonCutoff;
+
+    [JsonProperty("eta_cutoff")]
+    public float? fEtaCutoff;
+
+    [JsonProperty("guidance_scale")]
+    public float? fGuidanceScale;
+
+    [JsonProperty("negative_prompt")]
+    public string strNegativePrompt;
+
+    [JsonProperty("penalty_alpha")]
+    public float? fPenaltyAlpha;
+
+    [JsonProperty("mirostat_mode")]
+    public int? miroStatMode;
+
+    [JsonProperty("mirostat_tau")]
+    public float? fMiroStatTau;
+
+    [JsonProperty("mirostat_eta")]
+    public float? fMiroStatEta;
+
+    [JsonProperty("temperature_last")]
+    public bool? bTemperatureLast;
+
+    [JsonProperty("do_sample")]
+    public bool? bDoSample;
+
+    [JsonProperty("seed")]
+    public int? iSeed;
+
+    [JsonProperty("encoder_repetition_penalty")]
+    public float? fEncoderRepetitionPenalty;
+
+    [JsonProperty("no_repeat_ngram_size")]
+    public int? iNoRepeatNgramSize;
+
+    [JsonProperty("min_length")]
+    public int? iMinLength;
+
+    [JsonProperty("num_beams")]
+    public int? iNumBeams;
+
+    [JsonProperty("length_penalty")]
+    public float? fLengthPenalty;
+
+    [JsonProperty("early_stopping")]
+    public bool? bEarlyStopping;
+
+    [JsonProperty("truncation_length")]
+    public int? iTruncationLength;
+
+    [JsonProperty("max_tokens_second")]
+    public int? iMaxTokensSecond;
+
+    [JsonProperty("prompt_lookup_num_tokens")]
+    public int? iPromptLookupNumTokens;
+
+    [JsonProperty("custom_token_bans")]
+    public string? strCustomTokenBans;
+
+    [JsonProperty("auto_max_new_tokens")]
+    public bool? bAutoMaxNewTokens;
+
+    [JsonProperty("ban_eos_token")]
+    public bool? bBanEOSToken;
+
+    [JsonProperty("add_bos_token")]
+    public bool? bAddBOSToken;
+
+    [JsonProperty("skip_special_tokens")]
+    public bool? bSkipSpecialTokens;
+
+    [JsonProperty("grammar_string")]
+    public string strGrammarString;
 }
 
 [System.Serializable]
